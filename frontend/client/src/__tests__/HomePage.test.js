3916dd1a9df22c1d790fdb33b7dd7fa88c0fedfa
KimLe <kimle020102@gmail.com>
2024-06-22 08:41:33 +0700

    add some test

---
+++ frontend/client/src/__tests__/HomePage.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/customer/home/index'
+
+describe('HomePage', () => {
+    // test('renders list navigation', () => {
+    //     render(<Home />);
+    //     const listNavigation = screen.getByRole('navigation');
+    //     expect(listNavigation).toBeInTheDocument();
+    // });
+
+    test('renders "Tuyến đường phổ biến" section', () => {
+        render(<HomePage />);
+        const popularRoutesSection = screen.getByText('Tuyến đường phổ biến');
+        expect(popularRoutesSection).toBeInTheDocument();
+    });
+
+    test('renders "Cảm nhận của khách hàng" section', () => {
+        render(<HomePage />);
+        const customerReviewsSection = screen.getByText('Cảm nhận của khách hàng');
+        expect(customerReviewsSection).toBeInTheDocument();
+    });
+});
