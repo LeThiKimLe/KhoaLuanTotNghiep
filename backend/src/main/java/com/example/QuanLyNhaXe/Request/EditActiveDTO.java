0dfd17caa368a5c857a7940e12798a97a12a00a3
Nguyễn Thị Cẩm Nguyên <20110315@student.hcmute.edu.vn>
2024-06-26 23:20:02 +0700

    add api send otp for tel

--- backend/src/main/java/com/example/QuanLyNhaXe/Request/EditActiveDTO.java
+++ backend/src/main/java/com/example/QuanLyNhaXe/Request/EditActiveDTO.java
@@ -1,10 +1,16 @@
 package com.example.QuanLyNhaXe.Request;
 
+import com.example.QuanLyNhaXe.model.Staff;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EditActiveDTO {
 	private Integer id;
 	private boolean active;
